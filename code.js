ce83f0390531babf8fc7f1d8d586ae6fcd676f97
kkrweb <kanemkriz@gmail.com>
2025-01-28 16:12:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,13 @@
+// Kane Kriz
+// UWYO COSC 3020
+// 28 January 2025
+//
+// File Name: code.js
+// Modified from provided code.js file of "sum" 3020 Exercise.
+
+
 function sum(a) {
-    var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
